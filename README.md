549bb7c3569161b7746f3c3e6ce82754f3a553f2
HeinrichKing <heinrichgodinezk@gmail.com>
2025-03-26 20:56:55 -0500

    Añadido archivo config.toml para Streamlit y mejoras en la app

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# PROJECT7
-Repository of my TripleTen #7 Project
+# Proyecto de Visualización de Datos de Venta de Coches
+
+Este proyecto utiliza **Streamlit** para crear una aplicación web interactiva que permite explorar un conjunto de datos de anuncios de venta de coches. La aplicación proporciona dos tipos de gráficos:
+
+1. **Histograma**: Muestra la distribución de los valores de `odometer` (kilometraje) de los coches.
+2. **Gráfico de dispersión**: Muestra la relación entre el `odometer` y el `price` (precio) de los coches.
+
+Los usuarios pueden interactuar con la aplicación seleccionando casillas de verificación para visualizar estos gráficos.
+
+## Funcionalidades
+
+- **Mostrar histograma**: Permite ver la distribución del kilometraje de los coches.
+- **Mostrar gráfico de dispersión**: Permite visualizar la relación entre el kilometraje y el precio de los coches.
---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv(r"C:\Users\Usuario\Desktop\CARPETAS\TRIPLETEN\PROYECTO7\vehicles_us.csv") # leer los datos
+hist_checkbox = st.checkbox('Construir histograma') # crear un botón
+scater_checkbox= st.checkbox("Contruir gráfico de dispersión")
+        
+if hist_checkbox: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivosw
+    st.plotly_chart(fig, use_container_width=True)
+    
+if scater_checkbox:
+    
+    st.write("Creación de gráfico de dispersión para el conjunto de datos de anuncios de venta de coches")
+    
+    #Creamos el gráfico de dispersión 
+    fig1 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    
+    #Mostramos el gráfico de dispersión 
+    st.plotly_chart(fig1, use_container_width=True )
